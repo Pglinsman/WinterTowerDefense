7cf829da7b61c5b33fbe1167df9fb7b632427b21
Pglinsman <pglinsman@gmail.com>
2016-12-29 15:03:05 -0500

    Fixed!

---
+++ Winter TD/Assets/Enemy Prefabs/Scripts/EnemyScript.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    public float speed = 5f;
+
+    private Transform target;
+    private int waypoint = 0;
+
+    void Start()
+    {
+        target = Waypoints.waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector2 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector2.Distance(transform.position, target.position) <= 0.1f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (waypoint >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            waypoint++;
+            target = Waypoints.waypoints[waypoint];
+        }
+        
+    }
+}
